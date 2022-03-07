b1fc546a98dcaa29304e6d89df5d6cd4122757a8
Alexandr Kuizn <kineva1992@gmail.com>
2022-03-07 15:39:47 +0300

    Initial commit

---
+++ src/com/ky8/Multiply.java
@@ -0,0 +1,15 @@
+package com.ky8;
+
+import java.util.function.ToIntFunction;
+
+public class Multiply {
+    public static double multiply(Double a, Double b)
+    {
+
+        return a * b;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(multiply(1.2,2.3));
+    }
+}
