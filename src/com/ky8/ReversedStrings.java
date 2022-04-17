3196eb49e4fa5d514a867029a98173ea28d045a6
Alexandr Kuizn <kineva1992@gmail.com>
2022-04-17 16:17:42 +0300

    add new reasiation Cata Reversed Strings

--- src/com/ky8/ReversedStrings.java
+++ src/com/ky8/ReversedStrings.java
@@ -3,9 +3,28 @@ package com.ky8;
 //Complete the solution so that it reverses the string passed into it.
 public class ReversedStrings {
 
+    public static void main(String[] args)
+    {
+        ReversedStrings revers = new ReversedStrings();
+
+        System.out.println(revers.solution2("world"));
+    }
+
     public static String solution(String str) {
         StringBuilder bilder = new StringBuilder(str);
         bilder.reverse();
         return bilder.toString();
+    }
+
+    public String solution2(String str) {
+
+        String result = "";
+        for(int i = str.length() - 1; i >= 0; i--)
+        {
+            result += str.charAt(i);
+        }
+
+        return result;
+
     }
 }
