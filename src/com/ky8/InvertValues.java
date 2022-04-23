571abd9c698065e350551229d2b473f9ef8d93ff
Alexandr Kuizn <kineva1992@gmail.com>
2022-04-23 19:55:13 +0300

    add new Cata

--- src/com/ky8/InvertValues.java
+++ src/com/ky8/InvertValues.java
@@ -4,10 +4,14 @@ public class InvertValues {
     public static int[] invert(int[] array) {
 
         int[] resultArray = new int[array.length];
+        if(array == null || array.length == 0)
+            return new int[0];
+        else{
         for(int i = 0; i < array.length; i++)
         {
             resultArray[i] = array[i] * -1;
         }
         return resultArray;
+        }
     }
 }
