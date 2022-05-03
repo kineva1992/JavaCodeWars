a3418905e73252bdf74a0cc6a38ab5ddf8dc3f08
Alexandr Kuizn <kineva1992@gmail.com>
2022-05-03 17:56:30 +0300

    add new Cata

---
+++ src/com/ky7/BingoOrNot.java
@@ -0,0 +1,13 @@
+package com.ky7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class BingoOrNot {
+    public static String bingo(int[] numberArray){
+        var nums = Arrays.stream(numberArray).boxed().collect(Collectors.toList());
+        return "BINGO".chars().map(i -> i - 64).allMatch(nums::contains)? "WIN" : "LOSE";
+    }
+}
