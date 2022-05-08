097d1bd8bbdd08ace23744ddcc6ba9848eed9a73
Alexandr Kuizn <kineva1992@gmail.com>
2022-05-08 14:32:54 +0300

    add new Cata

--- src/com/ky7/FunWithListsLength.java
+++ src/com/ky7/FunWithListsLength.java
@@ -2,9 +2,9 @@ package com.ky7;
 
 /*Implement the method length, which accepts a linked list (head), and returns the length of the list.
 
-        For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
 
-        The linked list is defined as follows:*/
+The linked list is defined as follows:*/
 
 public class FunWithListsLength {
     public static int length(Node head) {
